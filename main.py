60f08f108d0c8232b9c2e7a0224cdd655e357dff
stellajslee <71096180+stellajslee@users.noreply.github.com>
2021-01-09 23:42:20 -0500

    Update main.py

--- main.py
+++ main.py
@@ -41,10 +41,10 @@ option = st.sidebar.selectbox('Emergencies', ('All', 'EMS', 'Fire', 'Traffic'))
 if option == 'All':
     # printing data
     if st.sidebar.button('Press to see total time spent for each location'):
-    st.sidebar.write("Total time from start (Hospital 1): " + str(totalTimeH1) + " mins")
-    st.sidebar.write("Total time from start (Hospital 2): " + str(totalTimeH2) + " mins")
-    st.sidebar.write("Total time from start (Fire Station 1): " + str(totalTimeF1) + " mins")
-    st.sidebar.write("Total time from start (Fire Station 2): " + str(totalTimeF2) + " mins")
+        st.sidebar.write("Total time from start (Hospital 1): " + str(totalTimeH1) + " mins")
+        st.sidebar.write("Total time from start (Hospital 2): " + str(totalTimeH2) + " mins")
+        st.sidebar.write("Total time from start (Fire Station 1): " + str(totalTimeF1) + " mins")
+        st.sidebar.write("Total time from start (Fire Station 2): " + str(totalTimeF2) + " mins")
 
         for x in range(0, 135):
             if data[x][4] == 'EMS:':
